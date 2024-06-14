0871ecf9980c32e1edf4902a202e585e5a920627
Chaitanyalonarkar <chaitanyalonarkar@gmail.com>
2024-06-14 23:10:46 +0530

    design home page

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -2,7 +2,9 @@ import React, { useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import toast, { Toaster } from "react-hot-toast";
-import Logout from "./Logout";
+import Logo from "../assets/logo.png";
+import il1 from "../assets/il1c.png";
+import tu from "../assets/illus.jpg";
 
 export default function Home() {
   const navigate = useNavigate();
@@ -13,7 +15,8 @@ export default function Home() {
         withCredentials: true,
       }); // Make sure the URL is correct
 
-      if (response.data.status) {
+      if (localStorage.getItem("user")) {
+        // if (response.data.status) {
         // toast.success(response.data.message || "User verified successfully");
         navigate("/");
       } else {
@@ -25,15 +28,139 @@ export default function Home() {
     }
   };
 
+  const blogs = [
+    {
+      title: "Why Enhancing Virtual Reality is Important",
+      description:
+        "Virtual reality is seen as a “fun” technology to some without much...",
+      thumbnail: "https://cdn-images-1.medium.com/max/2600/0*kz30LOdXT8CyOymh",
+      link: "https://medium.com/p/ac19dd21c728",
+      categories: [
+        "vr",
+        "technology",
+        "virtual-reality",
+        "engineering",
+        "artificial-intelligence",
+      ],
+      // publishedAt: Wed Aug 11 2021 18:43:34 GMT+0600
+    },
+    {
+      title: "How to Get Started With Data Science: a Brief Guide",
+      description:
+        "You’ve heard about data science and machine learning, and you want to get started. Maybe you hear...",
+      thumbnail: "https://cdn-images-1.medium.com/max/2600/0*Ah0vLtsvxqUvRWuS",
+      link: "https://medium.com/p/88ec244f2fee",
+      categories: [
+        "beginner-coding",
+        "data-science-training",
+        "machine-learning-course",
+      ],
+      // publishedAt: Mon Jul 26 2021 22:55:26 GMT+0600
+    },
+  ];
+
   useEffect(() => {
     verifyUser();
   }, []);
 
   return (
     <>
-      <h3>Home</h3>
-      <br />
-      <Logout />
+      <div className="home m-5 bg-white rounded-xl p-12 px-20  flex justify-around items-center ">
+        <div className="h-l flex flex-col gap-12">
+          <div className="h-text">
+            <div className="mb-4 font-medium text-indigo-900 text-lg opacity-75  ">
+              {/* <img src={Logo} alt="Logo" /> */}
+              Dive into Knowledge, Explore the World
+            </div>
+            <h1 className=" font-extrabold  spacing text-6xl text-indigo-400 ">
+              Inspire. Learn. Share.
+            </h1>
+            <h1 className=" font-extrabold  spacing text-5xl text-indigo-900 opacity-95 mt-5  leading-relaxed ">
+              A Blogs for Passionate People. <br />
+              {/* Your Journey Starts Here. */}
+            </h1>
+          </div>
+          <div className="join">
+            <Link
+              to="/login"
+              className="p-4 rounded-md bg-indigo-100 font-bold  hover:bg-indigo-200 text-indigo-700 "
+            >
+              {" "}
+              Join Us Today
+            </Link>
+          </div>
+        </div>
+        <div className="h-r w-2/4">
+          <div className="illus">
+            <img src={il1} alt="illustration" />
+          </div>
+        </div>
+      </div>
+
+      <div className="blog-section m-5 bg-white rounded-xl p-12 px-20 gap-12  flex flex-col justify-around items-center">
+        <div className="heading font-extrabold text-5xl flex flex-col items-center text-indigo-900 opacity-95">
+          Latest Blogs
+          <div className="h-1 bg-indigo-400 rounded w-3/4 justify-self-center mt-4"></div>
+        </div>
+
+        <div className="latestblogs ">
+          {/* {blogs.map((b) => {
+            <div>
+              <img src={b.thumbnail} alt="" />
+              <div>{b.categories}</div>
+              <div>{b.title}</div>
+              <div>{b.description}</div>
+            </div>;
+          })} */}
+
+          <div className="l-blog flex gap-12  ">
+            <div className=" w-2/4 rounded-xl overflow-hidden  ">
+              <img src={tu} alt="" />
+            </div>
+            <div className="w-2/4 flex flex-col  justify-center gap-7">
+              <div className="text-indigo-900 font-medium opacity-95 text-xl">
+                Cooking
+              </div>
+              <div className="font-bold text-4xl  text-indigo-900 opacity-95">
+                <Link to="">A Sweet Cake with Chocolate and Strawberry</Link>
+              </div>
+              <div className="text-slate-700 text-lg">
+                Warner Music Group announced today it’s acquiring the selected
+                assets of the music platform Songkick, including its app for
+                finding concerts and the company’s trademark. Songkick has been
+                involved in a lawsuit against the major…
+                <Link to="" className=" text-base text-sky-700">
+                  Read More
+                </Link>
+              </div>
+              <div>by Chaitanya Lonarkar,2 days ago</div>
+            </div>
+          </div>
+          <div className="l-blog flex flex-row-reverse gap-12  ">
+            <div className=" w-2/4 rounded-xl overflow-hidden  ">
+              <img src={tu} alt="" />
+            </div>
+            <div className="w-2/4 flex flex-col  justify-center gap-7">
+              <div className="text-indigo-900 font-medium opacity-95 text-xl">
+                Cooking
+              </div>
+              <div className="font-bold text-4xl  text-indigo-900 opacity-95">
+                <Link to="">A Sweet Cake with Chocolate and Strawberry</Link>
+              </div>
+              <div className="text-slate-700 text-lg">
+                Warner Music Group announced today it’s acquiring the selected
+                assets of the music platform Songkick, including its app for
+                finding concerts and the company’s trademark. Songkick has been
+                involved in a lawsuit against the major…
+                <Link to="" className=" text-base text-sky-700">
+                  Read More >>
+                </Link>
+              </div>
+              <div>by Chaitanya Lonarkar,2 days ago</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
